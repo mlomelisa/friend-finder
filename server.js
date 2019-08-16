5396dafd81ff5967644d9f99c30c8e1f8361f820
mlomelisa <monica.lomelim@gmail.com>
2019-08-15 22:53:05 -0700

    Complete survay html, sending the JSON objet

---
+++ server.js
@@ -0,0 +1,17 @@
+// Dependencies
+var express = require("express");
+var path = require("path");
+
+// Set up Express App
+var app = express();
+var PORT = 3000;
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Starts the server to begin listening
+// =============================================================
+app.listen(PORT, function() {
+  console.log("App listening on PORT " + PORT);
+});
